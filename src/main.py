c8c06676a6020cd7b237d6c09dea1a638c592cab
Rabonaa <rabemanantsoa.ma@gmail.com>
2026-05-25 16:03:52 +0200

    add dichotomie

--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-def g1(x):
-    return np.cos(x)
-
-def g2(x):
-    return np.log(2 + x)
 
 def point_fixe(g, x0, tol = 1e-10, err = 1, k = 0, max = 100, x_vals = None):
     x = x0
@@ -15,7 +10,7 @@ def point_fixe(g, x0, tol = 1e-10, err = 1, k = 0, max = 100, x_vals = None):
         x_new = g(x)
         x_vals.append(x_new)
         if abs(x_new - x) < tol:
-            print(f"Convergence réussie en {i+1} iterations.\n")
+            print(f"Convergence reussie en {i+1} iterations.\n")
             return x_new, x_vals
         x = x_new
     print("Iteration max atteinte, pas de convergence !\n")
@@ -25,7 +20,9 @@ def point_fixe(g, x0, tol = 1e-10, err = 1, k = 0, max = 100, x_vals = None):
 def erreur_pt_fixe(g, chemin):
     x_liste = np.array(chemin)
 
+    #|x_n+1 - x_n|
     erreur = np.abs(x_liste[1:] - x_liste[:-1])
+
     #|g(x_n) - x_n|
     x_n = x_liste[:-1]
     residu = np.abs(g(x_n) - x_n)
@@ -40,16 +37,51 @@ def erreur_pt_fixe(g, chemin):
     plt.grid(True, which="both", linestyle="-.")
     plt.show()
 
+def dichotomie(f, a = 0, b = 1, tol = 1e-6, max_iter = 100, k_bis = 0, errors_bis = []):
+    if f(a) * f(b) > 0:
+        print("Mauvais encadrement initial")
+        return None, None
+    else:
+        for i in range(max_iter):
+            m = (a+b)/2
+            errors_bis.append(m)
+
+            if abs(b - a)/2 < tol:
+                print(f"Convergence reussie en {i + 1} iterations\n")
+                return m, errors_bis
+            if f(a) * f(m) <= 0:
+                b = m
+            else:
+                a = m
+
+        print("Iteration max atteinte, pas de convergence !\n")
+        return m, errors_bis
+
+def erreur_dichotomie(f, milieux):
+
+
+
 def main():
-    #g(x) = cos(x) :
+    g1 = lambda x : np.cos(x)
+    g2 = lambda x : np.log(2 + x)
+    g3 = lambda x : 0.5 * (x + np.cos(x))
+
+    #g1:
     #solution, chemin = point_fixe(g1, 0.5)
 
-    #g(x) = ln(2 + x) :
-    solution, chemin = point_fixe(g2, 1, err = 2)
+    #g2:
+    #solution, chemin = point_fixe(g2, 1, err = 2)
+
+    #g3
+    """
+    solution, chemin = point_fixe(g3, 0.5, tol = 1e-8)
     chemin_float = [float(x) for x in chemin]
     print(f"Point fixe de g : {solution}")
     print(f"chemin : {chemin_float}")
-    erreur_pt_fixe(g2, chemin)
+    erreur_pt_fixe(g3, chemin)
+    """
+
+
 
 if __name__ == "__main__":
     main()
